77b1acc868d5ec491e85b735dca61e38fb0129c1
Fergus MacDermot <fergusmacdermot@gmail.com>
2019-11-10 22:01:43 +0800

    Adding teams and repo

--- prospero/github-repository.tf
+++ prospero/github-repository.tf
@@ -12,3 +12,33 @@ resource "github_repository" "team_baseline_configurations" {
   license_template = "mit"
   topics = ["propero", "config"]
 }
+
+# Protect the master branch of the repository.
+resource "github_branch_protection" "tools_config" {
+  repository     = "${github_repository.team_baseline_configurations.name}"
+  branch         = "master"
+  enforce_admins = true
+
+  #required_status_checks {
+  #  strict   = false
+  #  contexts = ["ci/travis"]
+  #}
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews = true
+    dismissal_teams       = ["${github_team.prospero.slug}"]
+    require_code_owner_reviews = true
+    required_approving_review_count = 1
+  }
+
+  restrictions {
+    teams = ["${github_team.prospero.slug}"]
+  }
+}
+
+# main prospero team has admin over own repos
+resource "github_team_repository" "tools_config_repo" {
+  team_id    = "${github_team.prospero.id}"
+  repository = "${github_repository.team_baseline_configurations.name}"
+  permission = "admin"
+}
