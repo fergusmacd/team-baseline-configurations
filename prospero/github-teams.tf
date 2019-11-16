52715e0f2afba13d1054e952ec21dc0a3aed97d8
Fergus MacDermot <fergusmacdermot@gmail.com>
2019-11-16 13:58:41 +0800

    Moving teams and members out of team folder

--- prospero/github-teams.tf
+++
@@ -1,5 +0,0 @@
-resource "github_team" "prospero" {
-  name        = "prospero"
-  description = "Team responsible for building out infrastructure changes and building developer tools."
-  privacy     = "closed"
-}
