32192652143a1a937a4a0e7de6aebb05987c3cbb
Fergus MacDermot <fergusmacdermot@gmail.com>
2019-11-19 19:59:32 +0800

    Update outputs for team viola

--- admin/outputs.tf
+++ admin/outputs.tf
@@ -6,6 +6,15 @@ output "team_id_prospero" {
   value = "${github_team.prospero.id}"
 }
 
+output "team_slug_viola" {
+  value = "${github_team.viola.slug}"
+}
+
+output "team_id_viola" {
+  value = "${github_team.viola.id}"
+}
+
+## members below here
 output "member_username_mononoke_bot" {
   value = "${github_membership.mononoke_bot.username}"
 }
