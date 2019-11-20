e2e398da35c03300a2a9eb3fc17c47db9bff0894
Fergus MacDermot <fergusmacdermot@gmail.com>
2019-11-20 20:51:47 +0800

    Change syntax to tf 12

--- prospero/github-team-membership.tf
+++ prospero/github-team-membership.tf
@@ -1,5 +1,5 @@
 resource "github_team_membership" "prospero_team_membership_mononke_bot" {
-  team_id  = "${data.terraform_remote_state.team.team_id_prospero}"
-  username = "${data.terraform_remote_state.team.member_username_mononoke_bot}"
+  team_id  = data.terraform_remote_state.team.team_id_prospero
+  username = data.terraform_remote_state.team.member_username_mononoke_bot
   role     = "member"
 }
