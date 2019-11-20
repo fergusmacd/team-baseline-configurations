69bd7103e69f2dcec87c63032cab223894dc7e2c
Fergus MacDermot <fergusmacdermot@gmail.com>
2019-11-20 21:04:42 +0800

    Adding checks to repo

--- prospero/github-repository.tf
+++ prospero/github-repository.tf
@@ -13,3 +13,33 @@ resource "github_repository" "team_baseline_configurations" {
   topics             = ["prospero", "config", "terraform"]
 }
 
+# Protect the master branch of the repository.
+resource "github_branch_protection" "team_baseline_config" {
+  repository     = "${github_repository.team_baseline_configurations.name}"
+  branch         = "master"
+  enforce_admins = true
+
+  required_status_checks {
+    strict   = false
+    contexts = ["atlas/mononoke/team-prospero",
+                "atlas/mononoke/organization-admin",]
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    dismissal_teams            = ["${data.terraform_remote_state.team.outputs.team_slug_prospero}"]
+    require_code_owner_reviews = false
+    # required_approving_review_count = 0
+  }
+
+  restrictions {
+    teams = ["${${data.terraform_remote_state.team.outputs.team_slug_prospero}}"]
+  }
+}
+
+# main prospero team has admin over own repos
+resource "github_team_repository" "team_baseline_config_repo" {
+  team_id    = "${${data.terraform_remote_state.team.outputs.team_id_prospero}}"
+  repository = "${github_repository.team_baseline_configurations.name}"
+  permission = "admin"
+}
